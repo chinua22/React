89b8281528e50b133fa5bc166372f87e0dd663d1
chinua22 <chinanuchiketa@yahoo.com>
2020-07-24 20:00:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import App from './Style.css';
 
-function App() {
+const imgUrls = [
+  "https://cmeimg-a.akamaihd.net/640/clsd/getty/c64f76dc20c246ca88ee180fe4b4b781",
+  "https://lh3.googleusercontent.com/oxPeODS2m6rYIVbhcQChRtOWEYeGDwbeeeB1cDU2o_WYAVPU61VIgx-_6BAh5gSL8Sw=h900",
+  "https://i0.wp.com/www.universodegatos.com/wp-content/uploads/2017/04/fivfelv7.jpg?resize=582%2C328",
+  "https://i.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg",
+  "https://ehealthforum.com/health/images/avatars/11699147425707699031013.jpeg",
+  
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImageIndex: 0
+    };
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.previousSlide = this.previousSlide.bind(this);
+  }
+
+  previousSlide() {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === 0;
+    const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  nextSlide() {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === lastIndex;
+    const index = shouldResetIndex ? 0 : currentImageIndex + 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  render() {
+    return (
+      <div className="carousel">
+        <Arrow direction="left" clickFunction={this.previousSlide} glyph="&#9664;" />
+        <ImageSlide url={imgUrls[this.state.currentImageIndex]} />
+        <Arrow direction="right" clickFunction={this.nextSlide} glyph="&#9654;" />
+      </div>
+    );
+  }
+}
+
+const Arrow = ({ direction, clickFunction, glyph }) => (
+  <div
+    className={`slide-arrow ${direction}`}
+    onClick={clickFunction}>
+    {glyph}
+  </div>
+);
+
+const ImageSlide = ({ url }) => {
+  const styles = {
+    backgroundImage: `url(${url})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="image-slide" style={styles}></div>
   );
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
 
 export default App;
